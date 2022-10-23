1eb87cdf84427500327b538d0f9c4cec2aeefc13
Gaston_Morel <gastonmorel210995@gmail.com>
2022-10-23 00:19:37 -0300

    primer commit

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Lemon pie",
+      precio: 250,
+      img:
+        "https://images.unsplash.com/photo-1583350229022-55a5fb986ead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=494&q=80",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Cheescake",
+      precio: 300,
+      img:
+        "https://images.unsplash.com/photo-1619985632461-f33748ef8f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Brownie",
+      precio: 150,
+      img:
+        "https://images.unsplash.com/photo-1590841609987-4ac211afdde1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Chocolate cake",
+      precio: 200,
+      img:
+        "https://tse4.mm.bing.net/th?id=OIP.dvjDzPNURDH-apLlMioL4wHaHa&pid=Api&P=0",
+      cantidad: 1,
+    },
+  ];
